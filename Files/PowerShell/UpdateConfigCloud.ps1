a214c9cb41d1e773a77d803e09e287872d550462
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2014-11-05 10:24:32 +0530

    fixup! Move to using ScriptFile resource.

--- Files/PowerShell/UpdateConfigCloud.ps1
+++ Files/PowerShell/UpdateConfigCloud.ps1
@@ -1,31 +1,36 @@
-param(
-    [string]$configPath,
-    [string]$certPath,
-    [string]$certPassword,
-    [string]$subscriptionId,
-    [string]$serviceName,
-    [string]$storageAccount,
-    [string]$storageAccountKey,
-    [string]$Mode
-    )
+function GetScript() {
+    param(
+        [string]$configPath
+        )
+    return @{ Configured = Select-String -path $configPath -pattern "cloud:" -allmatches -simplematch -quiet}
+}
 
-    function dsc_GET() {
-        return @{ Configured = Select-String -path $configPath -pattern "cloud:" -allmatches -simplematch -quiet}
+function SetScript() {
+    param(
+        [string]$configPath,
+        [string]$certPath,
+        [string]$certPassword,
+        [string]$subscriptionId,
+        [string]$serviceName,
+        [string]$storageAccount,
+        [string]$storageAccountKey
+         )
+    Write-Verbose "Configuring azure plugin in $configPath" -Verbose
+    Add-Content -path $configPath "`ncloud:`n`tazure:`n`t`tkeystore: $certPath`n`t`tpassword: $certPassword`n`t`tsubscription_id: $subscriptionId`n`t`tservice_name: $serviceName"
+    if( $storageAccountKey){ 
+        Write-Verbose "Storage key present; Configuring snapshot repository:  $configPath" -Verbose
+        Add-Content -path $configPath "`t`tstorage_account: $storageAccount`n`t`tstorage_key: $storageAccountKey"
     }
+}
 
-    function dsc_SET() {
-        Add-Content -path $configPath "`ncloud:`n`tazure:`n`t`tkeystore: $certPath`n`t`tpassword: $certPassword`n`t`tsubscription_id: $subscriptionId`n`t`tservice_name: $serviceName"
-        if( $storageAccountKey){ 
-            Add-Content -path $configPath "`t`tstorage_account: $storageAccount`n`t`tstorage_key: $storageAccountKey"
-        }
+function TestScript() {
+    param(
+        [string]$configPath
+         )
+    if (Select-String -path $configPath -pattern "cloud:" -allmatches -simplematch -quiet) {
+        Write-Verbose "Elastic Search Config already has Cloud settings" -Verbose
+        return $true
     }
+    return $false
+}
 
-    function dsc_TEST() {
-            if (Select-String -path $configPath -pattern "cloud:" -allmatches -simplematch -quiet) {
-                Write-Verbose "Elastic Search Config already has Cloud settings" -Verbose
-                    return $true
-            }
-            return $false
-    }
-
-    &"dsc_$Mode"
